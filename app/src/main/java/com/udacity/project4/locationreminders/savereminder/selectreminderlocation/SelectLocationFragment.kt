930c8251870892a5625d36c1ac40c3ea3038611c
Trifa20731 <45325028+TrifaC@users.noreply.github.com>
2022-02-27 00:26:19 +0800

    add location functions in the location selection fragment, navigation need to implement.

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -2,33 +2,35 @@ package com.udacity.project4.locationreminders.savereminder.selectreminderlocati
 
 
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
-import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
-import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.utils.Constants
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
-import org.koin.android.ext.android.bind
 import org.koin.android.ext.android.inject
+import java.util.*
+
+/**
+ * zoom to the user location after taking his permission
+ * add style to the map
+ * put a marker to location that the user selected
+ * call this function after the user confirms on the selected location
+ * */
 
 class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
@@ -36,12 +38,13 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         const val LOG_TAG: String = "SelectLocationFragment"
     }
 
+    //Google Mapp
     private lateinit var mMap: GoogleMap
+    private var selectedLocationName: String? = null
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -53,26 +56,35 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
+        setClickListeners()
 
         //add the map setup implementation
         val mapFragment = childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-//        TODO: zoom to the user location after taking his permission
-//        TODO: add style to the map
-//        TODO: put a marker to location that the user selected
-
-
-//        TODO: call this function after the user confirms on the selected location
-        onLocationSelected()
-
         return binding.root
     }
 
-    private fun onLocationSelected() {
+    private fun onLocationSelected(locationInfo: String) {
+
         //        TODO: When the user confirms on the selected location,
         //         send back the selected location details to the view model
         //         and navigate back to the previous fragment to save the reminder and add the geofence
+    }
+
+
+//------------------------------------- Click Functions --------------------------------------------
+
+
+    private fun setClickListeners() {
+        binding.savePoiBtn.setOnClickListener {
+            selectedLocationName?.let {
+                Toast.makeText(requireContext(), getString(R.string.toast_info_save_success), Toast.LENGTH_SHORT).show()
+                onLocationSelected(it)
+            }?:let{
+                Toast.makeText(requireContext(), getString(R.string.toast_please_choose_poi), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 
@@ -82,6 +94,82 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         Log.i(LOG_TAG, "onMapReady: Run.")
         mMap = googleMap
+
+        setMapZoomInHongKong()
+        setMapLongClick(mMap)
+        setMapPOIClick(mMap)
+        setMapStyle(mMap)
+        enableMyLocation()
+    }
+
+    /** Set the initial location to Hong Kong. */
+    private fun setMapZoomInHongKong() {
+        val homeLatLng = Constants.HONG_KONG_LATLNG
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, Constants.ZOOM_LEVEL))
+        mMap.addMarker(MarkerOptions().position(homeLatLng))
+    }
+
+    /**
+     * Set Up the Add Marker (Long Click) Function for the Google Map.
+     *
+     * @param map
+     * */
+    private fun setMapLongClick(map:GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            val snippet = String.format(
+            Locale.getDefault(),
+            "Current Locate: (%1$.5f, %2$.5f)",
+            latLng.latitude,
+            latLng.longitude
+        )
+            map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(getString(R.string.dropped_pin))
+                    .snippet(snippet)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+            )
+
+        }
+    }
+
+    /**
+     * Set up the Add Marker (Point Of Interest Click) Function for the Google Map. The previous POI
+     * marker will be removed and add the new one.
+     *
+     * @param map
+     * */
+    private fun setMapPOIClick(map:GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            map.clear()
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            poiMarker.showInfoWindow()
+            selectedLocationName = poi.name
+            Toast.makeText(requireContext(), "The location ${poi.name} has been chosen", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    /**
+     * Set up the style of the Google Map.
+     *
+     * @param map
+     * */
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            val success = map.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.map_style))
+
+            if (!success) {
+                Log.e(LOG_TAG, "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e(LOG_TAG, "Can't find style. Error: ", e)
+        }
     }
 
 
@@ -92,8 +180,14 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         inflater.inflate(R.menu.map_options, menu)
     }
 
+    /**
+     * The method selects the menu item and change the map type.
+     *
+     * @param item
+     * @return Boolean Value
+     * */
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // TODO: Change the map type based on the user's selection.
+        // Change the map type based on the user's selection.
         R.id.normal_map -> {
             Log.i(LOG_TAG, "The normal map has been chosen.")
             mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
@@ -118,7 +212,45 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
     }
 
 
-//--------------------------------------------------------------------------------------------------
+//------------------------------------- Permission Functions ---------------------------------------
+
+
+    /**
+     * Enable the current Location. If the permission deny, require for location permission.
+     * */
+    private fun enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) === PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(requireContext(), getString(R.string.location_jump_to_current), Toast.LENGTH_SHORT).show()
+            mMap.isMyLocationEnabled = true
+        }
+        else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                Constants.REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    /**
+     *
+     * */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        if (requestCode == Constants.REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            } else {
+                Toast.makeText(requireContext(), getString(R.string.location_permission_no_granted), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
 
 }
--- app/src/main/java/com/udacity/project4/utils/Constants.kt
+++ app/src/main/java/com/udacity/project4/utils/Constants.kt
@@ -1,6 +1,15 @@
 package com.udacity.project4.utils
 
+import com.google.android.gms.maps.model.LatLng
+
+
 object Constants {
+    // Activity Request Code.
     const val SIGN_IN_REQUEST_CODE: Int = 1997
     const val REQUEST_LOCATION_PERMISSION: Int = 1998
+
+    // Constant Map Setting Data.
+    val HONG_KONG_LATLNG: LatLng = LatLng(22.302711, 114.177216)
+    const val ZOOM_LEVEL: Float = 15f
+
 }
